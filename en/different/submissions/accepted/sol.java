e9921613b7bd850fe7fbf8eefb5fa2b3909d55c2
MengHuan Yu <cebrusfs@gmail.com>
2016-10-18 13:51:02 +0800

    fix different time limit

--- en/different/submissions/accepted/sol.java
+++ en/different/submissions/accepted/sol.java
@@ -1,12 +1,20 @@
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class sol {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNextLong()) {
-            long a = sc.nextLong();
-            long b = sc.nextLong();
+        String line;
+        while ((line = br.readLine()) != null)
+        {
+            String[] toks = line.split(" ");
+
+            long a = Long.parseLong(toks[0]);
+            long b = Long.parseLong(toks[1]);
+
             System.out.println(Math.abs(a-b));
         }
     }
